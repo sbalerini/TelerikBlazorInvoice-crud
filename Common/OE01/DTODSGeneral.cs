649ac6ac3409b090b0bfe0f36def0a2a2a0e68ba
Sebastian <sbalerini@integrasoftware.biz>
2024-11-25 09:38:27 -0300

    Se agrego el proyecto

---
+++ Common/OE01/DTODSGeneral.cs
@@ -0,0 +1,24 @@
+﻿
+namespace BlazorOE01.Common.OE01
+{
+    public class DTODSGeneral
+    {
+        public Dsgeneral dsCustomer { get; set; }
+    }
+
+    public class Dsgeneral
+    {
+        public TTCustomer[] ttCustomer { get; set; }
+    }
+
+    public class TTCustomer
+    {
+        public int CustNum { get; set; }
+        public string Country { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+
+    }
+}
+
